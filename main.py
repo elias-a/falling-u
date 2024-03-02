4cc644e8e182d8f4b9e1c81b13e731af090be923
elias-a <elias.alstead@gmail.com>
2024-03-01 23:45:27 -0500

    Set up U object visualization

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import os
-import tomllib
+import tomli
 from u import U
 from simulate import Simulation
-from visualize import visualize
+#from visualize import visualize
+import vpython
 
 
 try:
     with open(os.path.join(os.path.dirname(__file__), "config.toml"), "rb") as f:
-        config = tomllib.load(f)
+        config = tomli.load(f)
         dimensions_config = config["DIMENSIONS"]
         state_config = config["STATE"]
         base_length_m = config["DIMENSIONS"]["BASE_LENGTH_M"]
@@ -22,7 +23,7 @@ try:
 except Exception as e:
     raise Exception()
 
-_u = U(
+u_object = U(
     base_length_m=base_length_m,
     side_length_m=side_length_m,
     position_x_m=position_x_m,
@@ -32,7 +33,48 @@ _u = U(
     angular_position_rad=angular_position_rad,
     angular_velocity_rad_s=angular_velocity_rad_s)
 
-rate = 1000
-simulation = Simulation(_u, time_step_s=time_step_s)
-data = simulation.run(rate=rate)
-visualize(_u, data, rate=rate)
+#simulation = Simulation(_u, time_step_s=time_step_s)
+#data = simulation.run(rate=5)
+#visualize(_u, data, rate=5)
+
+vpython.scene.width = 500
+vpython.scene.height = 600
+vpython.scene.center = vpython.vec(0, u_object.position_m().y / 2, 0)
+thickness = 0.001
+
+ground = vpython.box(
+    pos=vpython.vec(0, 0, 0),
+    length=1,
+    height=0.01,
+    width=thickness)
+
+base = vpython.box(
+    pos=vpython.vec(
+        u_object.position_m().x,
+        u_object.position_m().y - u_object._center_of_mass_height_m,
+        0),
+    length=u_object._base_length_m,
+    height=thickness,
+    width = thickness,
+    up=vpython.vec(0, 1, 0))
+side_left = vpython.box(
+    pos=vpython.vec(
+        u_object.position_m().x - 0.5 * u_object._base_length_m,
+        u_object.position_m().y - u_object._center_of_mass_height_m + 0.5 * u_object._side_length_m,
+        0),
+    length=u_object._side_length_m,
+    height=thickness,
+    width=thickness,
+    up=vpython.vec(1, 0, 0))
+side_right = vpython.box(
+    pos=vpython.vec(
+        u_object.position_m().x + 0.5 * u_object._base_length_m,
+        u_object.position_m().y - u_object._center_of_mass_height_m + 0.5 * u_object._side_length_m,
+        0),
+    length=u_object._side_length_m,
+    height=thickness,
+    width=thickness,
+    up=vpython.vec(1, 0, 0))
+
+while True:
+    vpython.rate(60)
