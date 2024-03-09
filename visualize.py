82118e5c687abdc29ebd5e3e77df38709b1827ce
elias-a <elias.alstead@gmail.com>
2024-03-08 20:24:13 -0500

    Clean up visualization code

--- visualize.py
+++ visualize.py
@@ -1,14 +1,23 @@
 from vpython import scene, box, vec, rate
-from u import Vector
 
 
-def _create_box(length, point, thickness, up):
-    return box(
-        pos=vec(point.x, point.y, 0),
-        length=length,
-        height=thickness,
-        width = thickness,
-        up=up)
+def vec_2d(x, y):
+    return vec(x, y, 0)
+
+
+def _sides(p, d):
+    y = p.y - d.center_of_mass() + d.side_length_m / 2
+    left = vec_2d(x=p.x - d.base_length_m / 2, y=y)
+    right = vec_2d(x=p.x + d.base_length_m / 2, y=y)
+    return left, right
+
+
+def _base(p, d):
+    return vec_2d(x=p.x, y=p.y - d.center_of_mass())
+
+
+def _create_box(length, thickness, up):
+    return box(length=length, height=thickness, width = thickness, up=up)
 
 
 def visualize(dimensions, data):
@@ -25,25 +34,12 @@ def visualize(dimensions, data):
     # purely for visual purposes.
     thickness = 0.001
 
-    base = _create_box(
-        dimensions.base_length_m,
-        Vector(x=data[0].x, y=data[0].y - dimensions.center_of_mass()),
-        thickness,
-        vec(0, 1, 0))
-    side_left = _create_box(
-        dimensions.side_length_m,
-        Vector(
-            x=data[0].x - dimensions.base_length_m / 2,
-            y=data[0].y - dimensions.center_of_mass() + dimensions.side_length_m / 2),
-        thickness,
-        vec(1, 0, 0))
-    side_right = _create_box(
-        dimensions.side_length_m,
-        Vector(
-            x=data[0].x + dimensions.base_length_m / 2,
-            y=data[0].y - dimensions.center_of_mass() + dimensions.side_length_m / 2),
-        thickness,
-        vec(1, 0, 0))
+    base = _create_box(dimensions.base_length_m, thickness, vec(0, 1, 0))
+    left_side = _create_box(dimensions.side_length_m, thickness, vec(1, 0, 0))
+    right_side = _create_box(dimensions.side_length_m, thickness, vec(1, 0, 0))
+
+    base.pos = _base(data[0], dimensions)
+    left_side.pos, right_side.pos = _sides(data[0], dimensions)
 
     while True:
         rate(60)
